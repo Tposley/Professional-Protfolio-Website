a1044f4b11a69ba56d34dcc9a8643a0edbc72010
Timothy Posley <timothyposley@Timothys-MacBook-Pro.local>
2026-03-01 23:07:38 -0600

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,75 @@
+import {
+  c,
+  python,
+  java,
+  cpp,
+  javascript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  github,
+  git,
+  edunet,
+  weatherpedia,
+  termpw,
+  payloadmaster,
+  threejs,
+  mhft,
+  sketcher,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+export const services = [
+  { title: "C", icon: c },
+  { title: "C++", icon: cpp },
+  { title: "Python", icon: python },
+  { title: "Java", icon: java },
+];
+
+export const technologies = [
+  { name: "HTML 5", icon: html },
+  { name: "CSS 3", icon: css },
+  { name: "JavaScript", icon: javascript },
+  { name: "Rect JS", icon: reactjs },
+  { name: "Tailwind CSS", icon: tailwind },
+  { name: "Node JS", icon: nodejs },
+  { name: "Three JS", icon: threejs },
+  { name: "git", icon: git },
+];
+
+export const experiences = [
+  
+];
+
+export const projects = [
+  {
+    name: "Dress Me",
+    description:
+      "Web-based platform that allows users to create an online closet capable of creating and traking outfits",
+    tags: [
+      { name: "Flutter", color: "blue-text-gradient" },
+      { name: "python", color: "green-text-gradient" },
+      { name: "", color: "pink-text-gradient" },
+      
+    ],
+    image: github,
+    source_code_link: "https://github.com/lohitkolluri/WeatherPedia",
+  },
+  
+];
